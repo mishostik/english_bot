26b7f6efdbcc7febf8eed1d1cce4e171ee287b06
alexander <invral@yandex.ru>
2023-10-15 00:36:55 +0300

    fix

---
+++ config/config.go
@@ -0,0 +1,65 @@
+package config
+
+import (
+	"english_bot/pkg/secure"
+	"github.com/spf13/viper"
+	"log"
+)
+
+// TODO MONGO config
+type Config struct {
+	Postgres PostgresConfig
+	Telegram TelegramConfig
+}
+
+type PostgresConfig struct {
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"-"`
+	DBName   string `json:"DBName"`
+	SSLMode  string `json:"sslMode"`
+	PgDriver string `json:"pgDriver"`
+}
+
+type TelegramConfig struct {
+	Token string `json:"token"`
+}
+
+func LoadConfig() (*viper.Viper, error) {
+
+	viperInstance := viper.New()
+
+	viperInstance.AddConfigPath("./config")
+	viperInstance.SetConfigName("config")
+	viperInstance.SetConfigType("yml")
+
+	err := viperInstance.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+	return viperInstance, nil
+}
+
+func ParseConfig(v *viper.Viper) (*Config, error) {
+
+	var c Config
+
+	err := v.Unmarshal(&c)
+	if err != nil {
+		log.Fatalf("unable to decode config into struct, %v", err)
+		return nil, err
+	}
+	return &c, nil
+}
+
+func DecryptConfig(cfg *Config, s *secure.Shield) {
+
+	cfg.Postgres.Host = s.DecryptMessage(cfg.Postgres.Host)
+	cfg.Postgres.Port = s.DecryptMessage(cfg.Postgres.Port)
+	cfg.Postgres.User = s.DecryptMessage(cfg.Postgres.User)
+	cfg.Postgres.Password = s.DecryptMessage(cfg.Postgres.Password)
+	cfg.Postgres.DBName = s.DecryptMessage(cfg.Postgres.DBName)
+
+	cfg.Telegram.Token = s.DecryptMessage(cfg.Telegram.Token)
+}
